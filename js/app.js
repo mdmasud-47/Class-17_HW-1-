1681184dc39a8aaea3be88c80d89981b465f8105
mdmasud-47 <mdmoksod77@gmail.com>
2023-08-13 22:04:45 +0600

    ..

---
+++ js/app.js
@@ -0,0 +1,120 @@
+let math = 79.5;
+let bangla = 82;
+let english = 65;
+let logic = 34 ;
+let islamic = 100.01;
+let ict = 32 ;
+
+//math js 
+if(math>= 80 && math<=100) {
+    document.getElementById('math') .innerText = "A+";
+}else if ( math>=70 && math<80) {
+    document.getElementById('math') .innerText = "A";
+}else if ( math>=60 && math<70) {
+    document.getElementById('math') .innerText = "A-";
+}else if ( math>=50 && math<60) {
+    document.getElementById('math') .innerText = "B";
+}else if ( math>=40 && math<50) {
+    document.getElementById('math') .innerText = "C";
+}else if ( math>=33 && math<40) {
+    document.getElementById('math') .innerText = "D";
+}else if ( math>=0 && math<33) {
+    document.getElementById('math') .innerText = "F";
+}else{
+    document.getElementById('math') .innerText = ('Invalid Number');
+}
+
+//bangla js
+if(bangla>= 80 && bangla<=100) {
+    document.getElementById('bangla') .innerText = "A+";
+}else if(bangla>= 70 && bangla<80) {
+    document.getElementById('bangla') .innerText = "A";
+}else if(bangla>= 60 && bangla<70) {
+    document.getElementById('bangla') .innerText = "A-";
+}else if(bangla>= 50 && bangla<60) {
+    document.getElementById('bangla') .innerText = "B";
+} else if(bangla>= 40 && bangla<50) {
+    document.getElementById('bangla') .innerText = "C";
+}else if(bangla>= 33 && bangla<40) {
+    document.getElementById('bangla') .innerText = "D";
+}else if(bangla>= 0 && bangla<33) {
+    document.getElementById('bangla') .innerText = "F";
+}else{
+    document.getElementById('bangla') .innerText = ('Invalid Number');
+}
+
+//english js
+if(english>= 80 && english<=100) {
+    document.getElementById('english') .innerText = "A+";
+}else if(english>= 70 && english<80) {
+    document.getElementById('english') .innerText = "A";
+}else if(english>= 60 && english<70) {
+    document.getElementById('english') .innerText = "A-";
+}else if(english>= 50 && english<60) {
+    document.getElementById('english') .innerText = "B";
+}else if(english>= 40 && english<50) {
+    document.getElementById('english') .innerText = "C";
+}else if(english>= 33 && english<40) {
+    document.getElementById('english') .innerText = "D";
+}else if(english>= 0 && english<33) {
+    document.getElementById('english') .innerText = "F";
+}else{
+    document.getElementById('english') .innerText = ('Invalid Number');
+}
+
+//logic
+if(logic>= 80 && logic<=100) {
+    document.getElementById('logic') .innerText = "A+";
+}else if(logic>= 70 && logic<80) {
+    document.getElementById('logic') .innerText = "A";
+}else if(logic>= 60 && logic<70) {
+    document.getElementById('logic') .innerText = "A-";
+}else if(logic>= 50 && logic<60) {
+    document.getElementById('logic') .innerText = "B";
+}else if(logic>= 40 && logic<50) {
+    document.getElementById('logic') .innerText = "C";
+}else if(logic>= 33 && logic<40) {
+    document.getElementById('logic') .innerText = "D";
+}else if(logic>= 0 && logic<33) {
+    document.getElementById('logic') .innerText = "F";
+}else{
+    document.getElementById('logic') .innerText = ('Invalid Number');
+}
+
+//islamic
+if(islamic>= 80 && islamic<=100) {
+    document.getElementById('islamic') .innerText = "A+";
+}else if(islamic>= 70 && islamic<80) {
+    document.getElementById('islamic') .innerText = "A";
+}else if(islamic>= 60 && islamic<70) {
+    document.getElementById('islamic') .innerText = "A-";
+}else if(islamic>= 50 && islamic<60) {
+    document.getElementById('islamic') .innerText = "B";
+}else if(islamic>= 40 && islamic<50) {
+    document.getElementById('islamic') .innerText = "C";
+}else if(islamic>= 33 && islamic<40) {
+    document.getElementById('islamic') .innerText = "D";
+}else if(islamic>= 0 && islamic<33) {
+    document.getElementById('islamic') .innerText = "F";
+}else{
+    document.getElementById('islamic') .innerText = ('Invalid Number');
+}
+
+//ict
+if(ict>= 80 && ict<=100) {
+    document.getElementById('ict') .innerText = "A+";
+}else if(ict>= 70 && ict<80) {
+    document.getElementById('ict') .innerText = "A";
+}else if(ict>= 60 && ict<70) {
+    document.getElementById('ict') .innerText = "A-";
+}else if(ict>= 50 && ict<60) {
+    document.getElementById('ict') .innerText = "B";
+}else if(ict>= 40 && ict<50) {
+    document.getElementById('ict') .innerText = "C";
+}else if(ict>= 33 && ict<40) {
+    document.getElementById('ict') .innerText = "D";
+}else if(ict>= 0 && ict<33) {
+    document.getElementById('ict') .innerText = "F";
+}else{
+    document.getElementById('ict') .innerText = ('Invalid Number');
+}
